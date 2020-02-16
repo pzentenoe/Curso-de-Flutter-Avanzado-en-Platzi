d1c9f7a2b2ce5ad8d1bf09bf420b48f3e8f472ce
pzentenoe <pzentenoe@gmail.com>
2020-02-16 19:10:07 -0300

    integracion de generic bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trips_app/User/bloc/bloc_user.dart';
 
 import 'platzi_trips_cupertino.dart';
 
@@ -9,22 +11,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-
-          ),
-      //home: PlatziTrips()
-      home: PlatziTripsCupertino(),
+    return BlocProvider(
+      bloc: UserBloc(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(),
+        //home: PlatziTrips()
+        home: PlatziTripsCupertino(),
+      ),
     );
   }
 }
