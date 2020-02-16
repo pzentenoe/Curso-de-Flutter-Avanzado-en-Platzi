912c9b12e0828d277e1122ae12de3d0fd7f873af
pzentenoe <pzentenoe@gmail.com>
2020-02-16 18:41:19 -0300

    estructura de carpetas se pasa a clean arquitecture con bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'platzi_trips.dart';
+
 import 'platzi_trips_cupertino.dart';
 
 void main() => runApp(MyApp());
@@ -14,17 +12,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
 
-      ),
+          ),
       //home: PlatziTrips()
       home: PlatziTripsCupertino(),
     );
--- lib/platzi_trips.dart
+++ lib/platzi_trips.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'home_trips.dart';
-import 'search_trips.dart';
-import 'profile_trips.dart';
+
+import 'Place/ui/screens/home_trips.dart';
+import 'Place/ui/screens/search_trips.dart';
+import 'User/ui/screens/profile_trips.dart';
 
 class PlatziTrips extends StatefulWidget {
   @override
@@ -9,59 +10,32 @@ class PlatziTrips extends StatefulWidget {
     // TODO: implement createState
     return _PlatziTrips();
   }
-
 }
 
 class _PlatziTrips extends State<PlatziTrips> {
   int indexTap = 0;
-  final List<Widget> widgetsChildren = [
-    HomeTrips(),
-    SearchTrips(),
-    ProfileTrips()
-  ];
+  final List<Widget> widgetsChildren = [HomeTrips(), SearchTrips(), ProfileTrips()];
 
-  void onTapTapped(int index){
-
+  void onTapTapped(int index) {
     setState(() {
       indexTap = index;
     });
-
   }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
 
-
-    return
-
-      Scaffold(
+    return Scaffold(
       body: widgetsChildren[indexTap],
       bottomNavigationBar: Theme(
-        data: Theme.of(context).copyWith(
-          canvasColor: Colors.white,
-          primaryColor: Colors.purple
-        ),
-        child: BottomNavigationBar(
-          onTap: onTapTapped,
-          currentIndex: indexTap,
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                title: Text("")
-              ),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.search),
-                  title: Text("")
-              ),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.person),
-                  title: Text("")
-              ),
-            ]
-        ),
+        data: Theme.of(context).copyWith(canvasColor: Colors.white, primaryColor: Colors.purple),
+        child: BottomNavigationBar(onTap: onTapTapped, currentIndex: indexTap, items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("")),
+          BottomNavigationBarItem(icon: Icon(Icons.search), title: Text("")),
+          BottomNavigationBarItem(icon: Icon(Icons.person), title: Text("")),
+        ]),
       ),
     );
   }
-
 }
