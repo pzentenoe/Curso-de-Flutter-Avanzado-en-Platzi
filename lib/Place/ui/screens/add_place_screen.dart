e6a2a08966ba1c8bd1ea9d882d4dab46eec0418c
pzentenoe <pzentenoe@gmail.com>
2020-02-18 23:33:33 -0300

    Creacion de componente Title Header para add place screen

--- lib/Place/ui/screens/add_place_screen.dart
+++ lib/Place/ui/screens/add_place_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:platzi_trips_app/widgets/gradient_back.dart';
+import 'package:platzi_trips_app/widgets/title_header.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   File image;
@@ -15,6 +16,7 @@ class AddPlaceScreen extends StatefulWidget {
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Stack(
         children: <Widget>[
@@ -29,13 +31,28 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                   height: 45.0,
                   width: 45.0,
                   child: IconButton(
+                    iconSize: 30.0,
                     icon: Icon(Icons.keyboard_arrow_left),
+                    color: Colors.white,
                     onPressed: () {
                       Navigator.pop(context);
                     },
                   ),
                 ),
               ),
+              Flexible(
+                child: Container(
+                  width: screenWidth,
+                  padding: EdgeInsets.only(
+                    top: 45.0,
+                    left: 20.0,
+                    right: 10.0,
+                  ),
+                  child: TitleHeader(
+                    title: "Add a new Place",
+                  ),
+                ),
+              )
             ],
           )
         ],
